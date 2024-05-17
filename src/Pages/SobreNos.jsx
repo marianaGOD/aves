87e666e5d3768536a0a91117f8aa2e009e0f8c8f
Duarte Godinho <duarte.r.godinho@gmail.com>
2024-05-17 23:05:46 +0100

    sobre nos

--- src/Pages/SobreNos.jsx
+++ src/Pages/SobreNos.jsx
@@ -7,23 +7,34 @@ export default function SobreNos() {
       <div className="content-box">
         <h1>Sobre Nós</h1>
         <p>
-          Fundada em 1998, nossa organização é dedicada à conservação e ao
-          estudo das aves de rapina. Com uma equipe de especialistas apaixonados
-          e experientes, nos empenhamos em promover a educação ambiental e a
-          falcoaria como uma prática sustentável e respeitosa com a natureza.
+          A Falcoaria, considerada desde 2010 como Património Cultural
+          Intangível da Humanidade, consiste na arte de caça de presas
+          selvagens, através de uma parceria especial entre o homem e a ave de
+          presa. Esta prática é regulamentada pelo Instituto de Conservação da
+          Natureza e Florestas. Através desta arte, o falcoeiro consegue fazer
+          um eficaz Controlo de Avifauna, através de espantamento e captura de
+          algumas espécies infestantes, que prejudicam a saúde pública e/ou o
+          desenvolvimento de atividades económicas em determinado local.
+          Ecológica, natural e extremamente eficaz, com poucas baixas de
+          indivíduos, esta forma de controlo de espécies não causa impacto
+          ambiental nem nas restantes espécies, pois não são utilizados
+          quaisquer produtos tóxicos, nem armadilhas perigosas a outras espécies
+          e a humanos.
         </p>
         <p>
-          Ao longo dos anos, temos colaborado com diversas instituições
-          ambientais para garantir a preservação de habitats naturais e
-          contribuir para a recuperação de espécies em risco. Nosso centro
-          também oferece treinamento em falcoaria e manejo de aves de rapina,
-          proporcionando aos entusiastas e profissionais as habilidades
-          necessárias para cuidar adequadamente desses magníficos animais.
+          A <b>Aves do Oeste by TM</b> efetua também Apresentações de aves de
+          rapina, com demonstrações de voo e de características de cada espécie
+          e seus comportamentos naturais, com componente lúdico-pedagógica para
+          grupos, especialmente nas faixas etárias mais jovens, com o objetivo
+          de sensibilizar para a preservação das espécies e seus habitats.
         </p>
         <p>
-          Venha nos visitar ou entre em contato para saber mais sobre nosso
-          trabalho e como você pode contribuir ou aprender mais sobre falcoaria
-          e conservação.
+          Algumas das nossas aves estão sociabilizadas de modo a deixarem-se
+          tocar e acariciar, de modo a permitir ao público a aproximação e
+          contacto com estes animais selvagens, sempre com a maior segurança
+          para o público e para o animal. Mais informações sobre a Aves do
+          Oeste, bem como fotografias das nossas aves, podem ser encontradas nas
+          nossas redes sociais.
         </p>
       </div>
     </div>
